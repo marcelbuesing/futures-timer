bd7491fe894aae163233fe465a7933477e02e467
Travis CI User <travis@example.org>
2019-09-06 09:55:53 +0000

    Documentation for rustasync/futures-timer@92a0257

---
+++ implementors/core/marker/trait.Freeze.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["futures_timer"] = [{text:"impl Freeze for <a class=\"struct\" href=\"futures_timer/struct.Timer.html\" title=\"struct futures_timer::Timer\">Timer</a>",synthetic:true,types:["futures_timer::Timer"]},{text:"impl Freeze for <a class=\"struct\" href=\"futures_timer/struct.TimerHandle.html\" title=\"struct futures_timer::TimerHandle\">TimerHandle</a>",synthetic:true,types:["futures_timer::TimerHandle"]},{text:"impl Freeze for <a class=\"struct\" href=\"futures_timer/struct.Delay.html\" title=\"struct futures_timer::Delay\">Delay</a>",synthetic:true,types:["futures_timer::delay::Delay"]},{text:"impl Freeze for <a class=\"struct\" href=\"futures_timer/struct.Interval.html\" title=\"struct futures_timer::Interval\">Interval</a>",synthetic:true,types:["futures_timer::interval::Interval"]},{text:"impl Freeze for <a class=\"struct\" href=\"futures_timer/struct.SetDefaultError.html\" title=\"struct futures_timer::SetDefaultError\">SetDefaultError</a>",synthetic:true,types:["futures_timer::SetDefaultError"]},{text:"impl&lt;F&gt; Freeze for <a class=\"struct\" href=\"futures_timer/ext/struct.Timeout.html\" title=\"struct futures_timer::ext::Timeout\">Timeout</a>&lt;F&gt; <span class=\"where fmt-newline\">where<br>&nbsp;&nbsp;&nbsp;&nbsp;F: Freeze,&nbsp;</span>",synthetic:true,types:["futures_timer::ext::Timeout"]},{text:"impl&lt;S&gt; Freeze for <a class=\"struct\" href=\"futures_timer/ext/struct.TimeoutStream.html\" title=\"struct futures_timer::ext::TimeoutStream\">TimeoutStream</a>&lt;S&gt; <span class=\"where fmt-newline\">where<br>&nbsp;&nbsp;&nbsp;&nbsp;S: Freeze,&nbsp;</span>",synthetic:true,types:["futures_timer::ext::TimeoutStream"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
